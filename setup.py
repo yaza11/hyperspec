0bcca4e4d39f785dcba16166fdf6128206cf567b
yaza11 <116643078+yaza11@users.noreply.github.com>
2025-11-19 11:03:35 +0100

    fix requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.1'
+VERSION = '0.1.0'
 DESCRIPTION = 'Processing of hyperspectral data.'
 NAME = "hyperspec"
 
@@ -26,7 +26,7 @@ setup(
                       'numpy>=2.0.0',
                       'tqdm',
                       'scikit-image',
-                      'spectral'
+                      'spectral',
                       'h5py'],
     extras_require={'dev': 'twine'},
     keywords=['python', 'hyperspectral imaging'],
